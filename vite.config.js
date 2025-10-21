62fc5c2ca043201165c6753e874f91b95a077419
Jesús Prado <jes_us100@hotmail.com>
2025-10-21 13:32:11 -0500

    Proyecto listo para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // Reemplaza "nombre-del-repositorio" con el nombre exacto de tu repo en GitHub
 export default defineConfig({
-  base: "/datosmaccelular.github.io/",
+  base: "/",
   plugins: [react()],
 });
 
